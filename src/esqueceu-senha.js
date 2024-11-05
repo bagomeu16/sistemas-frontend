7be6189d616740c74fa0dfabbdf7c42afef14dfd
Jean de Godoy <jean16omg@gmail.com>
2024-11-05 19:37:44 -0300

    adicionando arquivos do sistema frontend

---
+++ src/esqueceu-senha.js
@@ -0,0 +1,62 @@
+﻿window.onload = function (e) {
+
+    var btnRecuperarSenha = document.getElementById("btnRecuperarSenha");
+
+    var txtEmail = document.getElementById("txtEmail");
+
+    txtEmail.focus();
+    btnRecuperarSenha.onclick = function (e) {
+
+        e.preventDefault();
+
+        var email = txtEmail.value;
+
+        if (email == "") {
+            exibirMensagemErro("Campo E-mail obrigatório.");
+        }
+        else {
+            enviarEmailRecuperacao(email);
+        }
+    };
+
+    function exibirMensagemErro(mensagem) {
+
+        var spnErro = document.getElementById("spnErro");
+
+        spnErro.innerText = mensagem;
+        spnErro.style.display = "block";
+
+        setTimeout(function () {
+            spnErro.style.display = "none";
+        }, 5000);
+    };
+
+    function enviarEmailRecuperacao(email) {
+
+        var data = JSON.stringify({
+            "email": email
+        });
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+
+                var esqueceuSenhaResult = JSON.parse(this.responseText);
+
+                if (esqueceuSenhaResult.sucesso) {
+                   alert('E-mail enviado com sucesso!')
+                }
+                else {
+                    exibirMensagemErro(result.mensagem);
+                }
+            }
+        });
+
+        xhr.open("POST", "https://localhost:44310/api/usuario/esqueceuSenha");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+    };
+}
---
+++ src/home.js
@@ -0,0 +1,61 @@
+﻿window.onload = function (e) {
+
+    var usuarioGuid = localStorage.getItem("usuarioGuid");
+
+    if (usuarioGuid == null) {
+        window.location.href = "login.html";
+    }
+    else {
+        obterUsuario(usuarioGuid);
+    }
+
+    var lnkSair = document.getElementById("lnkSair");
+
+    lnkSair.onclick = function (e) {
+
+        localStorage.removeItem("usuarioGuid");
+
+        window.location.href = "login.html";
+    }
+
+    var icon = document.querySelector(".icon");
+
+    icon.onclick = function (e) {
+
+        var menu = document.querySelector(".topnav");
+
+        if (menu.className == "topnav") {
+            menu.className += " open";
+        }
+        else {
+            menu.className = "topnav";
+        }
+    }
+
+    function obterUsuario(usuarioGuid) {
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+
+                var result = JSON.parse(this.responseText);
+
+                if (result.sucesso) {
+                    //sucesso
+                    var spnMensagem = document.getElementById("spnMensagem");
+
+                    spnMensagem.innerText = "Bem-vindo ao sistema " + result.nome + "! ";
+                }
+                else {
+                    window.location.href = "login.html";
+                }
+            }
+        });
+
+        xhr.open("GET", "https://localhost:44310/api/usuario/obterUsuario?usuarioGuid=" + usuarioGuid);
+
+        xhr.send();
+    }
+
+}
